910cf430fb484c3a3bcd2ca6c719064dc727bde9
Alexander Koptev <askoptevwork@gmail.com>
2025-04-13 23:42:02 +0300

    18.5. State и реактивности

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
-'use strict'
-import './app.css'
+"use strict";
+
+import { MainView } from "./views/main/main";
+
+class App {
+  routes = [{ path: "", view: MainView }];
+  appState = {
+    favorites: [],
+  }
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this));
+    this.route();
+  }
+
+  route() {
+    if (this.currentView) {
+      this.currentView.destroy();
+    }
+    console.log(location.hash);
+
+    const view = this.routes.find((r) => r.path == location.hash).view;
+    this.currentView = new view(this.appState);
+    this.currentView.render();
+  }
+}
+
+new App();
