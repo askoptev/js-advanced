45bd8ca6336a8a126853dc3cd8df923e2d87e6f3
Alexander Koptev <askoptevwork@gmail.com>
2025-04-15 20:18:11 +0300

    18.10. Упражнение - Состояние загрузки

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -2,6 +2,7 @@ import { AbstractView } from "../../common/view";
 import onChange from "on-change";
 import { Header } from "../../components/header/header";
 import { Search } from "../../components/search/search";
+import { CardList } from "../../components/card-list/card-list";
 
 export class MainView extends AbstractView {
   state = {
@@ -26,14 +27,21 @@ export class MainView extends AbstractView {
   }
 
   async stateHook(path) {
-    console.log(path);
     if (path == "searchQuery") {
       this.state.loading = true;
       const data = await this.loadList(this.state.searchQuery, this.state.offset);
       this.state.loading = false;
       this.state.list = data.docs;
     }
+    if (path === 'list') {
+      this.render();
+    }
+    if (path === "loading") {
+      this.render();
+    }
   }
+
+
 
   async loadList(q, offset) {
     const res = await fetch(`https://openlibrary.org/search.json?q=${q}&offset=${offset}`);
@@ -43,6 +51,7 @@ export class MainView extends AbstractView {
   render() {
     const main = document.createElement("div");
     main.append(new Search(this.state).render());
+    main.append(new CardList(this.appState, this.state).render());
     this.app.innerHTML = "";
     this.app.append(main);
     this.renderHeader();
