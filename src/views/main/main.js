d1ebf1481624dc6129665adf5c0bee6f00903257
Alexander Koptev <askoptevwork@gmail.com>
2025-04-15 21:03:38 +0300

    18.11. Вёрстка карточки

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -10,6 +10,7 @@ export class MainView extends AbstractView {
     loading: false,
     searchQuery: undefined,
     offset: 0,
+    numFound: 0,
   };
 
   constructor(appState) {
@@ -31,6 +32,8 @@ export class MainView extends AbstractView {
       this.state.loading = true;
       const data = await this.loadList(this.state.searchQuery, this.state.offset);
       this.state.loading = false;
+      console.log(data)
+      this.state.numFound = data.numFound;
       this.state.list = data.docs;
     }
     if (path === 'list') {
