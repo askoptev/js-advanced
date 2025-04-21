634680217119ff85b1c885268f076791c7c6174d
Alexander Koptev <askoptevwork@gmail.com>
2025-04-21 20:34:49 +0300

    18.14. Финал

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -9,30 +9,56 @@ export class Book extends DivComponent {
     publication: "1954",
     pages: "1193",
     key: "OL22856696M",
+    desc: "A Game of Thrones is the first novel in A Song of Ice and Fire, a series of fantasy novels by the American author George R. R. Martin. It was first published on August 1, 1996. The novel won the 1997 Locus Award and was nominated for both the 1997 Nebula Award and the 1997 World Fantasy Award. The novella Blood of the Dragon, comprising the Daenerys Targaryen chapters from the novel, won the 1997 Hugo Award for Best Novella. In January 2011, the novel became a New York Times Bestseller and reached No. 1 on the list in July 2011.",
+    tags: ["Fiction", "Fantasy fiction", "English Fantasy fiction", "Wizards"],
   };
   constructor(appState) {
     super();
     this.appState = appState;
+    this.#getBook(this.appState.keyBook);
   }
 
-  #getBook() {}
+  // Функция имиитрирует загрузку данных о книге
+  async #getBook(keyBook) {
+    const result = await fetch(`https://${keyBook}`);
+    if (result.ok) {
+      this.state.fetchData = await result.json();
+      this.render();
+    }
+  }
 
   render() {
-    console.log(this.appState.favorites[0].cover_edition_key);
-    
-    const existInFavorites = this.appState.favorites.find((b) => b.key == this.state.key);
+    const existInFavorites = this.appState.favorites.find((b) => b.cover_edition_key.includes(this.state.key));
     this.el.classList.add("book");
     this.el.innerHTML = `
       <h1>${this.state.title}</h1>
       <div class="book__card">
         <img src="https://covers.openlibrary.org/b/olid/${this.appState.keyBook}-M.jpg" alt="Обложка">
         <div class="book__data">
-          <div class="book__data-text"><span>Автор:</span><span>${this.state.author}</span></div>
-          <div class="book__data-text"><span>Category:</span><span>${this.state.category}</span></div>
-          <div class="book__data-text"><span>Первая публикация:</span><span>${this.state.publication}</span></div>
-          <div class="book__data-text"><span>Число страниц:</span><span>${this.state.pages}</span></div>
+          <div class="book__data-text"><span class="book__data-text-title">Автор:</span><span class="book__data-text-text">${
+            this.state.author
+          }</span></div>
+          <div class="book__data-text"><span class="book__data-text-title">Category:</span><span class="book__data-text-text">${
+            this.state.category
+          }</span></div>
+          <div class="book__data-text"><span class="book__data-text-title">Первая публикация:</span><span class="book__data-text-text">${
+            this.state.publication
+          }</span></div>
+          <div class="book__data-text"><span class="book__data-text-title">Число страниц:</span><span class="book__data-text-text">${
+            this.state.pages
+          }</span></div>
           <button class="book__favorites-add">${existInFavorites ? "Из избранного" : "В избранное"}</button>
-          </div>
+        </div>
+      </div>
+      <div class="book__desc">
+        <h2 class="book__desc-title">Описание:</h2>
+        <p class="book__desc-text">${this.state.desc}</p>
+      </div>
+      <div class="book__chips">        
+        <h2 class="book__chips-title">Теги:</h2>
+        <ui class="book__chips-list">
+        ${this.state.tags.map((item) => `<li><button class="book__chip-button">${item}</button></li>`).join("")}          
+        </ui>
       </div>
     `;
     return this.el;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,7 +15,7 @@ export class Header extends DivComponent {
         <img src="/static/logo.svg" alt="Logo">
       </div>
       <div class="menu">
-        <a class="menu__item" href="?#">
+        <a class="menu__item" href="#">
           <img src="/static/search.svg" alt="Поиск иконка"/>
           Поиск книг
         </a>
